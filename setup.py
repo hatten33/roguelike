82bd019d813b6303e92ac333b696563b3e88adaa
Will Butler <hatten33@gmail.com>
2022-01-01 03:18:59 -0800

    0.0.2 cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrogue",
-    version="0.0.1",
+    version="0.0.2",
     author="Will Butler",
     author_email="will@aerenserve.net",
     description="An extensible nethack style roguelike",
